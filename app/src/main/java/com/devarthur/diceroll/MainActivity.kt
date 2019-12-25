44ab31157f81fdf74a35dc29b5023c5e720517ed
Arthur <arthur.gomes_4718@hotmail.com>
2019-12-25 18:02:00 -0200

    Using databing for viewmodel actions

--- app/src/main/java/com/devarthur/diceroll/MainActivity.kt
+++ app/src/main/java/com/devarthur/diceroll/MainActivity.kt
@@ -14,20 +14,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var diceImage: ImageView
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
-    //TODO create a view model and bind data using data binding inside layout XML
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         viewModel = MainViewModel()
-
-        binding.rollButton.text = getString(R.string.lets_roll)
-        binding.rollButton.setOnClickListener {
-            viewModel.rolldice()
-        }
-
         viewModel.currentDiceResource.observe(this, Observer { imageResource ->
             binding.diceImage.setImageResource(imageResource)
         })
